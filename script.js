9be8c3a0dac256bea04a5361636e9662e279e123
Roman Troshin <roman.troshin98@gmail.com>
2022-05-02 14:51:35 +0300

    feat: create container with textarea

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.createElement('div');
+const textarea = document.createElement('textarea');
+container.classList.add('container');
+textarea.setAttribute('id', 'textarea');
+container.appendChild(textarea);
+document.body.appendChild(container);
