12a91ef1275b99f88012b074acfad9fc0be24065
Roman Troshin <roman.troshin98@gmail.com>
2022-05-05 21:29:17 +0300

    refactor: delete buttons class

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -1,5 +1,3 @@
-import Buttons from './Buttons.js';
-
 export default class Keyboard {
   constructor(textarea, keyButtons) {
     this.keyboardBody = document.createElement('div');
@@ -7,17 +5,160 @@ export default class Keyboard {
     this.textarea = textarea;
     this.keyButtons = keyButtons;
     this.buttons = '';
+    this.capsLock = false;
     this.keys = [];
   }
 
   initKeyboard() {
-    this.keyButtons.forEach((itemButtons) => {
-      // const keysClass = items[0];
-      // const keysEng = items[1].en;
-      // const keysRus = items[1].ru;
-      this.buttons = new Buttons(this.keyboardBody, itemButtons, this.textarea);
-      this.keyboardBody.appendChild(this.buttons.initKeys());
-    });
+    this.keyboardBody.appendChild(this.initKeys());
+    this.keys = this.keyboardBody.children;
     return this.keyboardBody;
+  }
+
+  initKeys() {
+    const fragment = document.createDocumentFragment();
+    this.keyButtons.forEach((elem) => {
+      this.keyElement = document.createElement('div');
+      this.keyElement.className = 'keyboard__key';
+      this.keyElement.classList.add(`${elem.code}`);
+      fragment.appendChild(this.keyElement);
+      this.setKeyButtons(elem);
+    });
+
+    return fragment;
+  }
+
+  setKeyButtons(elem) {
+    switch (elem.code) {
+      case 'Backspace':
+        this.setBackspace(elem);
+        break;
+
+      case 'Tab':
+        this.setTab(elem);
+        break;
+
+      case 'Delete':
+        this.setDelete(elem);
+        break;
+
+      case 'Enter':
+        this.setEnter(elem);
+        break;
+
+      case 'CapsLock':
+        this.setCapsLock(elem);
+        break;
+
+      default:
+        this.keyElement.textContent = elem.key.en;
+
+        this.keyElement.addEventListener('mousedown', (e) => {
+          e.target.classList.add('active');
+        });
+
+        this.keyElement.addEventListener('mouseup', (e) => {
+          e.target.classList.remove('active');
+          if (this.capsLock) {
+            this.textarea.value += elem.key.en.toUpperCase();
+          } else {
+            this.textarea.value += elem.key.en.toLowerCase();
+          }
+        });
+
+        break;
+    }
+  }
+
+  setBackspace(elem) {
+    this.keyElement.textContent = elem.key.en;
+
+    this.keyElement.addEventListener('mousedown', (e) => {
+      e.target.classList.add('active');
+    });
+
+    this.keyElement.addEventListener('mouseup', (e) => {
+      e.target.classList.remove('active');
+      this.textarea.value = this.textarea.value.substring(0, this.textarea.value.length - 1);
+      this.textarea.textContent = this.textarea.value;
+    });
+    return this.keyElement;
+  }
+
+  setTab(elem) {
+    this.keyElement.textContent = elem.key.en;
+
+    this.keyElement.addEventListener('mousedown', (e) => {
+      e.target.classList.add('active');
+    });
+
+    this.keyElement.addEventListener('mouseup', (e) => {
+      e.target.classList.remove('active');
+      this.textarea.value += '  ';
+      this.textarea.textContent = this.textarea.value;
+    });
+
+    return this.keyElement;
+  }
+
+  setDelete(elem) {
+    this.keyElement.textContent = elem.key.en;
+
+    this.keyElement.addEventListener('mousedown', (e) => {
+      e.target.classList.add('active');
+    });
+
+    this.keyElement.addEventListener('mouseup', (e) => {
+      e.target.classList.remove('active');
+      this.textarea.value = this.textarea.value.substring(1);
+      this.textarea.textContent = this.textarea.value;
+    });
+
+    return this.keyElement;
+  }
+
+  setEnter(elem) {
+    this.keyElement.textContent = elem.key.en;
+
+    this.keyElement.addEventListener('mousedown', (e) => {
+      e.target.classList.add('active');
+    });
+
+    this.keyElement.addEventListener('mouseup', (e) => {
+      e.target.classList.remove('active');
+      this.textarea.value += '\n';
+      this.textarea.textContent = this.textarea.value;
+    });
+
+    return this.keyElement;
+  }
+
+  setCapsLock(elem) {
+    this.keyElement.textContent = elem.key.en;
+
+    this.keyElement.addEventListener('mousedown', (e) => {
+      this.toggleCapsLock();
+      e.target.classList.add('active');
+    });
+
+    this.keyElement.addEventListener('mouseup', (e) => {
+      e.target.classList.remove('active');
+    });
+
+    return this.keyElement;
+  }
+
+  toggleCapsLock() {
+    this.capsLock = !this.capsLock;
+
+    for (let i = 0; i < this.keys.length; i += 1) {
+      if (this.keys[i].childElementCount === 0) {
+        if (this.capsLock) {
+          this.keys[i].textContent = this.keys[i].textContent.toUpperCase();
+        } else {
+          this.keys[i].textContent = this.keys[i].textContent.toLowerCase();
+        }
+      }
+    }
   }
 }
