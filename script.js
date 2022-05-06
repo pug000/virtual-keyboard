04b89684593aeafc7ffbf2def34b734d40bdc319
Roman Troshin <roman.troshin98@gmail.com>
2022-05-07 01:45:53 +0300

    refactor: change styles

--- script.js
+++ script.js
@@ -2,29 +2,14 @@ import Container from './js/Container.js';
 import Keyboard from './js/Keyboard.js';
 import Textarea from './js/Textarea.js';
 import keyButtons from './keyButtons.js';
+import Background from './js/Background.js';
 
 const container = new Container();
+const background = new Background();
 const textarea = new Textarea();
 const keyboard = new Keyboard(textarea.initTextarea(), keyButtons);
 
+document.body.appendChild(background.initBackground());
 document.body.appendChild(container.init());
 container.init().appendChild(textarea.initTextarea());
 container.init().appendChild(keyboard.initKeyboard());
-
-
-
-// Array.from(keyboard.keys).forEach((elem) => {
-//   document.addEventListener('keydown', (e) => {
-//     if (elem.classList.contains(`${e.code}`)) {
-//       elem.classList.add('active');
-//     }
-//   });
-//   document.addEventListener('keyup', (e) => {
-//     if (elem.classList.contains(`${e.code}`)) {
-//       elem.classList.remove('active');
-//     }
-//   });
-// });
-
-
-// document.onke
