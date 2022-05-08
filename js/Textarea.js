ef1b9b71183fdface858ffcd886ea3f0d87bb9df
Roman Troshin <roman.troshin98@gmail.com>
2022-05-08 17:06:11 +0300

    refactor: styles for textarea

--- js/Textarea.js
+++ js/Textarea.js
@@ -2,7 +2,8 @@ export default class Textarea {
   constructor() {
     this.textarea = document.createElement('textarea');
     this.textarea.setAttribute('id', 'textarea');
-    this.textarea.value = 'adasda';
+    this.textarea.setAttribute('spellcheck', 'false');
+    this.textarea.placeholder = 'Клавиатура создана в операционной системе Windows. \nПереключение языка: Shift + Alt.';
   }
 
   initTextarea() {
